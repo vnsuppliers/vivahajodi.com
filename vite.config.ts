0807d4d7744d657698e2ff346e3153856204f213
Accounts@adisys.net <Accounts@adisys.net>
2026-08-03 11:20:40 +0530

    Nest js api issue

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://matrimony-api-source.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
